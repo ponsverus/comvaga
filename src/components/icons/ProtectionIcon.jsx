e14d26acb3f2af43f359fbcec8abb04ba8376750
Everton Rebeca <ponsverus@gmail.com>
2026-09-21 08:45:26 -0300

    Refactor ShieldIcon to delete style prop
    Removed style prop from ShieldIcon component.

--- src/components/icons/ProtectionIcon.jsx
+++ src/components/icons/ProtectionIcon.jsx
@@ -1,4 +1,6 @@
-export default function ShieldIcon({ className = '', title, style = {}, ...props }) {
+export default function ShieldIcon({ className = '', title,
+  ...props }) {
+  delete props.style;
   const shieldPath =
     'M12 2.5C9.5 2.5 6.5 3.5 5 4.5V12.5C5 16.5 8 20 12 21.5C16 20 19 16.5 19 12.5V4.5C17.5 3.5 14.5 2.5 12 2.5Z';
 
@@ -7,8 +9,7 @@ export default function ShieldIcon({ className = '', title, style = {}, ...props
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       className={className}
-      style={{ display: 'inline-block', ...style }}
-      aria-hidden={title ? undefined : 'true'}
+            aria-hidden={title ? undefined : 'true'}
       role={title ? 'img' : 'presentation'}
       {...props}
     >
